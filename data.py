32fb30385e19b913edd37fc48a93456dcada8855
Ryuichiro Chiba <ryuichiro.chiba@gmail.com>
2017-10-18 18:11:14 +0900

    Refactor codes of data.py

--- data.py
+++ data.py
@@ -15,45 +15,20 @@ def perform_pca(data, dim):
     pca = PCA(n_components=dim)
     return pca.fit_transform(data)
 
-class MnistDataset:
-    def __init__(self, code_dim, code_init, color=False):
-        from tensorflow.examples.tutorials.mnist import input_data
-        mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
-        self.train_images = mnist.train.images
-        self.train_images = np.reshape(self.train_images, [len(self.train_images), 28, 28, 1])
-        self.train_labels = mnist.train.labels
-        self.train_size = len(self.train_images)
-        self.test_images = mnist.test.images
-        self.test_images = np.reshape(self.test_images, [len(self.test_images), 28, 28, 1])
-        self.test_labels = mnist.test.labels
-        self.images = np.concatenate((self.train_images, self.test_images))
-        self.labels = np.concatenate((self.train_labels, self.test_labels))
-
-        if code_init == None:
-            self.codes = None
-        elif code_init == 'gaussian':
-            self.codes = np.random.randn(len(self.images), code_dim)
-        elif code_init == "pca":
-            print('processing pca...')
-            self.codes = perform_pca(self.images, code_dim)
-            print('done.')
-        else:
-            print('load codes from ' + code_init)
-            self.load_codes(code_init)
-            print('done.')
-
-        if self.codes is not None:
-            self.codes /= np.linalg.norm(self.codes, axis=1, keepdims=True)
-
-        if color == True:
-            self.images = np.asarray([np.tile(image(1, 1, 3)) for image in self.images])
-
-        if self.codes is not None:
-            self.train_codes = self.codes[:self.train_size]
-            self.test_codes = self.codes[self.train_size:]
-
+class DatasetBase(object):
+    def __init_(self):
+        self.images = np.empty(1)
+        self.train_images = np.empty(1)
+        self.test_images = np.empty(1)
+        self.train_size = 0
+        self.labels = np.empty(1)
+        self.train_labels = np.empty(1)
+        self.test_labels = np.empty(1)
+        self.codes = np.empty(1)
+        self.train_codes = np.empty(1)
+        self.test_codes = np.empty(1)
         self.read_idx = -1
-        self.rand_idx = np.random.permutation(self.train_size)
+        self.rand_idx = None
 
     def save_codes(self, pkl_path):
         self.codes.dump(pkl_path)
@@ -103,7 +78,47 @@ class MnistDataset:
             else:
                 return self.test_images[idx], self.test_labels[idx]
 
-class Cifar10Dataset:
+class MnistDataset(DatasetBase):
+    def __init__(self, code_dim, code_init, color=False):
+        from tensorflow.examples.tutorials.mnist import input_data
+        mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
+        self.train_images = mnist.train.images
+        self.train_images = np.reshape(self.train_images, [len(self.train_images), 28, 28, 1])
+        self.train_labels = mnist.train.labels
+        self.train_size = len(self.train_images)
+        self.test_images = mnist.test.images
+        self.test_images = np.reshape(self.test_images, [len(self.test_images), 28, 28, 1])
+        self.test_labels = mnist.test.labels
+        self.images = np.concatenate((self.train_images, self.test_images))
+        self.labels = np.concatenate((self.train_labels, self.test_labels))
+
+        if code_init == None:
+            self.codes = None
+        elif code_init == 'gaussian':
+            self.codes = np.random.randn(len(self.images), code_dim)
+        elif code_init == "pca":
+            print('processing pca...')
+            self.codes = perform_pca(self.images, code_dim)
+            print('done.')
+        else:
+            print('load codes from ' + code_init)
+            self.load_codes(code_init)
+            print('done.')
+
+        if self.codes is not None:
+            self.codes /= np.linalg.norm(self.codes, axis=1, keepdims=True)
+
+        if color == True:
+            self.images = np.asarray([np.tile(image(1, 1, 3)) for image in self.images])
+
+        if self.codes is not None:
+            self.train_codes = self.codes[:self.train_size]
+            self.test_codes = self.codes[self.train_size:]
+
+        self.read_idx = -1
+        self.rand_idx = np.random.permutation(self.train_size)
+
+class Cifar10Dataset(DatasetBase):
     def __init__(self, dataset_path, code_dim, code_init):
         self.images = None
         self.labels = None
@@ -181,54 +196,6 @@ class Cifar10Dataset:
         self.read_idx = -1
         self.rand_idx = np.random.permutation(self.train_size)
 
-    def save_codes(self, pkl_path):
-        self.codes.dump(pkl_path)
-
-    def load_codes(self, pkl_path):
-        with open(pkl_path, 'rb') as f:
-            self.codes = cPickle.load(f)
-
-    def train_batch(self, size, with_idx=False):
-        idx = self.rand_idx[self.read_idx+1 : self.read_idx+1 + size]
-
-        if self.read_idx+1 + size*2 > self.train_size+1:
-            last_batch = True
-            self.read_idx = -1
-            self.rand_idx = np.random.permutation(self.train_size)
-        else:
-            last_batch = False
-            self.read_idx += size
-
-        if with_idx:
-            if self.codes is not None:
-                return self.train_images[idx], self.train_labels[idx],\
-                    self.train_codes[idx], idx, last_batch
-            else:
-                return self.train_images[idx], self.train_labels[idx],\
-                    idx, last_batch
-        else:
-            if self.codes is not None:
-                return self.train_images[idx], self.train_labels[idx],\
-                    self.train_codes[idx], last_batch
-            else:
-                return self.train_images[idx], self.train_labels[idx],\
-                    last_batch
-
-    def test_batch(self, size, with_idx=False):
-        idx = np.random.permutation(len(self.images)-self.train_size)[:size]
-        if with_idx:
-            if self.codes is not None:
-                return self.test_images[idx], self.test_labels[idx],\
-                    self.test_codes[idx], idx
-            else:
-                return self.test_images[idx], self.test_labels[idx], idx
-        else:
-            if self.codes is not None:
-                return self.test_images[idx], self.test_labels[idx],\
-                    self.test_codes[idx]
-            else:
-                return self.test_images[idx], self.test_labels[idx]
-
     def _unpickle(self, file_path):
         f = open(file_path, 'rb')
         data = cPickle.load(f)
@@ -242,7 +209,7 @@ class Cifar10Dataset:
         x = encoder.fit_transform(x).toarray()
         return x
 
-class CelebADataset:
+class CelebADataset(DatasetBase):
     def __init__(self, dataset_path, code_dim, code_init):
         self.images = None
         self.codes = None
@@ -285,52 +252,4 @@ class CelebADataset:
 
         self.read_idx = -1
         self.rand_idx = np.random.permutation(self.train_size)
-
-    def save_codes(self, pkl_path):
-        self.codes.dump(pkl_path)
-
-    def load_codes(self, pkl_path):
-        with open(pkl_path, 'rb') as f:
-            self.codes = cPickle.load(f)
-
-    def train_batch(self, size, with_idx=False):
-        idx = self.rand_idx[self.read_idx+1 : self.read_idx+1 + size]
-
-        if self.read_idx+1 + size*2 > self.train_size+1:
-            last_batch = True
-            self.read_idx = -1
-            self.rand_idx = np.random.permutation(self.train_size)
-        else:
-            last_batch = False
-            self.read_idx += size
-
-        if with_idx:
-            if self.codes is not None:
-                return self.train_images[idx], self.train_labels[idx],\
-                    self.train_codes[idx], idx, last_batch
-            else:
-                return self.train_images[idx], self.train_labels[idx],\
-                    idx, last_batch
-        else:
-            if self.codes is not None:
-                return self.train_images[idx], self.train_labels[idx],\
-                    self.train_codes[idx], last_batch
-            else:
-                return self.train_images[idx], self.train_labels[idx],\
-                    last_batch
-
-    def test_batch(self, size, with_idx=False):
-        idx = np.random.permutation(len(self.images)-self.train_size)[:size]
-        if with_idx:
-            if self.codes is not None:
-                return self.test_images[idx], self.test_labels[idx],\
-                    self.test_codes[idx], idx
-            else:
-                return self.test_images[idx], self.test_labels[idx], idx
-        else:
-            if self.codes is not None:
-                return self.test_images[idx], self.test_labels[idx],\
-                    self.test_codes[idx]
-            else:
-                return self.test_images[idx], self.test_labels[idx]
 
